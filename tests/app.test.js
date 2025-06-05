b780f8f59af8725028c473b1a9a10b566bdac385
Joel-glitch-alt <joeladdition@gmail.com>
2025-06-05 14:57:23 +0000

    update

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,6 +4,9 @@ const app = require("../app");
 
 describe("GET /", () => {
   it("should return home page", async () => {
+    // const res = await request(app).get("/");
+    // expect(res.statusCode).toBe(200);
+    // expect(res.text).toContain("Welcome");
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
     expect(res.text).toContain("Welcome");
